84443dd8906de9f8dffe98a889c7894a0daef325
Cysiuu <cysiuu021@protonmail.com>
2025-03-08 18:03:33 +0100

    Fix invalid foreign keys declaration

--- database/migrations/2025_03_08_164041_create_tasks_table.php
+++ database/migrations/2025_03_08_164041_create_tasks_table.php
@@ -13,13 +13,17 @@ return new class() extends Migration {
             $table->id();
             $table->string("title");
             $table->string("description");
-            $table->integer("difficulty_level")->references("difficulty_level")->on("task_difficulties");
-            $table->string("reset_frequency")->references("id")->on("task_reset_configs");
+            $table->integer("difficulty_level");
+            $table->string("reset_frequency");
             $table->dateTime("due_date");
             $table->boolean("is_completed")->default(false);
             $table->boolean("is_deadline_task")->default(false);
             $table->float("experience_reward")->default(0);
             $table->timestamps();
+
+            // Foreign key constraints
+            $table->foreign("difficulty_level")->references("difficulty_level")->on("task_difficulties");
+            $table->foreign("reset_frequency")->references("id")->on("task_reset_configs");
         });
     }
 
