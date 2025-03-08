27c5a58555f931afa0098c44fc34f8880738c148
Cysiuu <cysiuu021@protonmail.com>
2025-03-08 18:03:58 +0100

    Add migration for task difficulties table

---
+++ database/migrations/2025_03_08_165837_create_task_difficulties_table.php
@@ -0,0 +1,25 @@
+<?php
+
+declare(strict_types=1);
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class() extends Migration {
+    public function up(): void
+    {
+        Schema::create("task_difficulties", function (Blueprint $table): void {
+            $table->integer("difficulty_level")->primary();
+            $table->float("energy_cost");
+            $table->float("health_penalty");
+            $table->float("base_exp_multiplier");
+            $table->timestamps();
+        });
+    }
+
+    public function down(): void
+    {
+        Schema::dropIfExists("task_difficulties");
+    }
+};
