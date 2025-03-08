e9c765bfe3e9d43b61bb06687670916ce755a818
Cysiuu <cysiuu021@protonmail.com>
2025-03-08 18:14:45 +0100

    Add migration for user tasks table

---
+++ database/migrations/2025_03_08_171051_create_user_tasks_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('user_tasks', function (Blueprint $table) {
+            $table->id();
+            $table->integer('user_id');
+            $table->integer('task_id');
+            $table->timestamp('completed_at')->nullable();
+            $table->timestamp('reset_at')->nullable();
+            $table->float('progress')->default(0);
+            $table->timestamps();
+
+            # Foreign keys
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+            $table->foreign('task_id')->references('id')->on('tasks')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('user_tasks');
+    }
+};
