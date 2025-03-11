9a605d8a95bf6fe2ba388b8e6888cbae5e83545c
Cysiuu <cysiuu021@protonmail.com>
2025-03-11 01:12:03 +0100

    Add Task Factory

---
+++ database/factories/TaskFactory.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Task>
+ */
+class TaskFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            "title" => $this->faker->sentence,
+            "description" => $this->faker->paragraph,
+            "difficulty_level" => $this->faker->numberBetween(1, 4),
+            "reset_frequency" => $this->faker->numberBetween(0, 6),
+            "due_date" => $this->faker->dateTimeBetween("now", "+1 year"),
+            "is_completed" => $this->faker->boolean,
+            "is_deadline_task" => $this->faker->boolean,
+            "experience_reward" => $this->faker->randomFloat(2, 1, 100),
+        ];
+    }
+}
