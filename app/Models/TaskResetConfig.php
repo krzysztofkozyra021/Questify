6bda82a59c53564c69e35a5d6e199e94dab3517a
Cysiuu <cysiuu021@protonmail.com>
2025-03-11 01:20:22 +0100

    Add Task Reset Config Factory

--- app/Models/TaskResetConfig.php
+++ app/Models/TaskResetConfig.php
@@ -6,6 +6,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class TaskResetConfig extends Model
 {
---
+++ database/factories/TaskResetConfigFactory.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\TaskResetConfig>
+ */
+class TaskResetConfigFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            "reset_frequency" => $this->faker->numberBetween(0, 6),
+            "reset_time" => $this->faker->dateTimeBetween("now", "+1 year"),
+        ];
+    }
+}
