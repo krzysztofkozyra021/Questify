aec33ea3ecdf3a13be5c5ff5fd1914e23f099351
Krzysztof Kozyra <cysiuu021@protonmail.com>
2025-05-12 15:29:40 +0200

    Add test admin account for development

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -5,8 +5,9 @@ declare(strict_types=1);
 namespace Database\Seeders;
 
 use App\Models\User;
+use App\Models\UserStatistics;
 use Illuminate\Database\Seeder;
-
+use Illuminate\Support\Facades\Hash;
 class UserSeeder extends Seeder
 {
     /**
@@ -15,5 +16,26 @@ class UserSeeder extends Seeder
     public function run(): void
     {
         User::Factory(30)->create();
+
+        $admin = User::create([
+            'name' => 'Admin',
+            'email' => 'admin@admin.com',
+            'password' => Hash::make('admin'),
+            'is_admin' => true,
+        ]);
+
+        $admin->userStatistics()->create([
+            'user_id' => $admin->id,
+            'class' => 1,
+            'current_health' => 100,
+            'current_energy' => 100,
+            'max_health' => 100,
+            'max_energy' => 100,
+            'current_experience' => 0,
+            'next_level_experience' => 100,
+            'level' => 1,
+            'energy_regen_rate' => 1,
+        ]);
+
     }
 }
