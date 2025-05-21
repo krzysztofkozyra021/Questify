3cd16a2a7f6c904dd96a1440865a7446c368c387
Krzysztof Kozyra <cysiuu021@protonmail.com>
2025-05-21 17:37:54 +0200

    style: animation when stats are reduced

--- resources/js/Components/PlayerPanel.vue
+++ resources/js/Components/PlayerPanel.vue
@@ -98,8 +98,8 @@ const experiencePercentage = computed(() => {
         <svg xmlns="http://www.w3.org/2000/svg" class="h-3 w-3 md:h-4 md:w-4 text-red-500" viewBox="0 0 24 24" fill="currentColor">
           <path d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z" />
         </svg>
-        <div class="w-24 md:w-32 h-2 md:h-3 bg-gray-100 overflow-hidden">
-          <div class="h-full bg-red-500" :style="{ width: ((currentPlayerHealth / maxPlayerHealth * 100) || 0) + '%' }"></div>
+        <div class="w-24 md:w-32 h-2 md:h-3 bg-gray-100 overflow-hidden rounded-full">
+          <div class="h-full bg-red-500 transition-all duration-500 ease-in-out rounded-full" :style="{ width: ((currentPlayerHealth / maxPlayerHealth * 100) || 0) + '%' }"></div>
         </div>
         <span class="text-xs md:text-sm text-white font-normal">{{ currentPlayerHealth }}/{{ maxPlayerHealth }}</span>
       </div>
@@ -108,8 +108,8 @@ const experiencePercentage = computed(() => {
         <svg xmlns="http://www.w3.org/2000/svg" class="h-3 w-3 md:h-4 md:w-4 text-blue-500" viewBox="0 0 24 24" fill="currentColor">
           <path d="M13 10V3L4 14h7v7l9-11h-7z" />
         </svg>
-        <div class="w-24 md:w-32 h-2 md:h-3 bg-gray-100 overflow-hidden">
-          <div class="h-full bg-blue-500" :style="{ width: ((currentPlayerEnergy / maxPlayerEnergy * 100) || 0) + '%' }"></div>
+        <div class="w-24 md:w-32 h-2 md:h-3 bg-gray-100 overflow-hidden rounded-full">
+          <div class="h-full bg-blue-500 transition-all duration-500 ease-in-out rounded-full" :style="{ width: ((currentPlayerEnergy / maxPlayerEnergy * 100) || 0) + '%' }"></div>
         </div>
         <span class="text-xs md:text-sm text-white font-normal">{{ currentPlayerEnergy }}/{{ maxPlayerEnergy }}</span>
       </div>
@@ -118,8 +118,8 @@ const experiencePercentage = computed(() => {
         <svg xmlns="http://www.w3.org/2000/svg" class="h-3 w-3 md:h-4 md:w-4 text-amber-400" viewBox="0 0 24 24" fill="currentColor">
           <path d="M11.049 2.927c.3-.921 1.603-.921 1.902 0l1.519 4.674a1 1 0 00.95.69h4.915c.969 0 1.371 1.24.588 1.81l-3.976 2.888a1 1 0 00-.363 1.118l1.518 4.674c.3.922-.755 1.688-1.538 1.118l-3.976-2.888a1 1 0 00-1.176 0l-3.976 2.888c-.783.57-1.838-.197-1.538-1.118l1.518-4.674a1 1 0 00-.363-1.118l-3.976-2.888c-.784-.57-.38-1.81.588-1.81h4.914a1 1 0 00.951-.69l1.519-4.674z" />
         </svg>
-        <div class="w-24 md:w-32 h-2 md:h-3 bg-gray-100 overflow-hidden">
-          <div class="h-full bg-amber-400" :style="{ width: ((currentPlayerExperience / maxPlayerExperience * 100) || 0) + '%' }"></div>
+        <div class="w-24 md:w-32 h-2 md:h-3 bg-gray-100 overflow-hidden rounded-full">
+          <div class="h-full bg-amber-400 transition-all duration-500 ease-in-out rounded-full" :style="{ width: ((currentPlayerExperience / maxPlayerExperience * 100) || 0) + '%' }"></div>
         </div>
         <span class="text-xs md:text-sm text-white font-normal">{{ currentPlayerExperience }}/{{ maxPlayerExperience }}</span>
       </div>
@@ -158,6 +158,12 @@ const experiencePercentage = computed(() => {
 
 .group:hover {
   transform: translateY(-1px);
+}
+
+/* Add smooth transition for the stats bars */
+.transition-all {
+  transition-property: all;
+  transition-timing-function: cubic-bezier(0.4, 0, 0.2, 1);
 }
 
 @media (max-width: 640px) {
